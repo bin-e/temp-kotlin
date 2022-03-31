5e89ac963eec2e6339944afa8b4a6529775038ea
Jeong-Uk Park <dnr1105@gmail.com>
2022-03-31 22:31:26 +0900

    Solve programmers 86051

--- src/Solution.kt
+++ src/Solution.kt
@@ -1,6 +1,13 @@
 class Solution {
-    fun process(str: String?) {
-        str?.let { /*Do something*/ }
+    fun solution(numbers: IntArray): Int {
+        var answer: Int = -1
+
+        for(x in 1..9){
+            if(numbers.contains(x)) continue
+            else answer += x
+        }
+
+        return answer
     }
 }
 
