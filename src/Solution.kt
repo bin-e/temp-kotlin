9ee26a1f176e13698b26a230fcdecd5137db8301
Jeong-Uk Park <dnr1105@gmail.com>
2022-04-09 17:10:25 +0900

    화난다.. 다했는데

--- src/Solution.kt
+++ src/Solution.kt
@@ -1,9 +1,41 @@
 class Solution {
-    fun solution(n: Int): Int {
-        var answer: Int = n
+    fun solution(call: String): String {
+        var answer: String = ""
 
-        for( x in 1..n) {
-            if( n%x == 1 && answer > x ) answer = x
+        var splitter = 1
+//        val pattern = mutableListOf<String>()
+        var pattern = mutableMapOf<String, Int>()
+        while (splitter < call.length) {
+            for (i in 0..call.length step splitter) {
+                var counterTemp = 0
+                var temp = if (i + splitter >= call.length + 1) continue//call.substring(i, call.length) // 길이 초과
+                else call.substring(i, i + splitter)
+
+                for (j in call.indices step splitter) {
+                    if (j + splitter > call.length) break
+                    if (call.substring(j, j + splitter) == temp) {
+
+                        ++counterTemp
+                    }
+                }
+
+                if (pattern.containsKey(temp)) continue
+                pattern[temp] = counterTemp // add subStrings
+            }
+
+            splitter++
+        }
+
+        val lastPatterns = mutableListOf<String>()
+        pattern.forEach { t, u ->
+            if (u >= pattern.values.toList().maxOrNull()!!) lastPatterns.add(t)
+        }
+
+        val minCounter = call.length
+        lastPatterns.forEach {
+            val temp = call.replace(it, "")
+
+            if (temp.length < minCounter) answer = temp
         }
 
         return answer
@@ -11,5 +43,6 @@ class Solution {
 }
 
 fun main() {
-    val temp: Int = 4
+    val temp = Solution().solution("abcabcdefabc")
+    println(temp)
 }
